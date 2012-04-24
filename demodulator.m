6f2fab6d60a8a899cc39a7a7280eecd8b222620a
Geoffrey Lawler <gslawler@gmail.com>
2012-04-23 22:43:36 -0400

    Added stop signal function, might have some bugs

--- demodulator.m
+++ demodulator.m
@@ -8,7 +8,8 @@ function [ img ] = demodulator( wavFile, imgFile )
 WAV_FS = 16000; %Hz
 WAV_NBITS = 16; %bits/sample
 JPEG_NBITS = 8; %bits
-JPEG_WIDTH = 800; %pixels
+IMG_WIDTH = 800; %pixels
+SAMP_PIX = 10; %samples / pixel
 
 BLACK = 1500; %Hz
 WHITE = 2300; %Hz
@@ -20,12 +21,9 @@ STOP_FREQ = 450; %Hz
 
 FFT_LEN = 256;
 
-w = zeros(10,1); %[0.21; 0.45; 0.72; 0.85; 1; 1; 0.85; 0.72; 0.45; 0.21];
-
-% construct a Hamming window for use with STFT
-for i = 0:9
-   w(i+1) = 0.54-0.46*cos(2*pi*i/9); 
-end
+%% Functions
+HamWindow = @(n, N) .54-.46*cos(2*pi.*n/(N-1));
+w = HamWindow(0:SAMP_PIX - 1, SAMP_PIX)';
 
 %% Read in .wav file
 [signal, fs, ~] = wavread(wavFile);
@@ -44,18 +42,17 @@ plot(f, y);
 xlabel('Frequency Hz'); ylabel('FFT signal')
 
 len = length(signal);
-img_pixels = len/10;
-img_width = 800;
-img_height = img_pixels/img_width;
+img_pixels = len/SAMP_PIX;
+img_height = img_pixels/IMG_WIDTH;
 
 f = (0:(FFT_LEN-1))*(WAV_FS/FFT_LEN);
 
-img = zeros(img_width,img_height);
+img = zeros(IMG_WIDTH, img_height);
 
 for i=1:img_height
-    for j=1:img_width
-        start_index = 10*((i-1)*img_width + (j-1) ) + 1;
-        chunk = signal(start_index:start_index+9).*w;
+    for j=1:IMG_WIDTH
+        start_index = SAMP_PIX*((i-1)*IMG_WIDTH + (j-1) ) + 1;
+        chunk = signal(start_index:start_index + SAMP_PIX - 1).*w;
         Y = fft(chunk,FFT_LEN);
         freq = f(Y == max(Y(1:(FFT_LEN/2))));
         color = min((freq - BLACK)/(WHITE-BLACK),1);
--- modulator.m
+++ modulator.m
@@ -4,13 +4,14 @@ function modulator(imgFile, wavFile)
 %   into a 16-bit, single channel .wav file with sampling frequency, fs =
 %   16 KHz. The .wav file will be formatted according to the NOAA Marine
 %   Weather Fax Format.
-
+clc; clear;
 %% Constants
 WAV_FS = 16000; %Hz
 WAV_TS = 1/WAV_FS; %seconds
 WAV_NBITS = 16; %bits/sample
 JPEG_NBITS = 8; %bits
 JPEG_WIDTH = 800; %pixels
+SAMP_PIX = 10; %samples / pixel
 
 BLACK = 1500; %Hz
 WHITE = 2300; %Hz
@@ -19,6 +20,18 @@ START_TIME = 5; %seconds
 START_FREQ = 300; %Hz
 STOP_TIME = 5; %seconds
 STOP_FREQ = 450; %Hz
+
+%% Functions
+    % --- Encodes black/white pixels varying at freq for given duration.
+    function y = startStop(freq, duration)
+        colors = [BLACK WHITE];
+        n = 0 : duration * WAV_FS - 1;
+        samp = 1/(2*freq) * WAV_FS; %Samples per black/white pixel
+        color = mod(floor(n / samp), 2); %Divide into even, odd colors 
+        y = .99*cos(2*pi*colors(color+1).*n.*WAV_TS);
+    end
+% y = startStop(START_FREQ, 5);
+% wavwrite(y, WAV_FS, WAV_NBITS,'start2.wav');
 
 %% Read in Image
 img = imread(imgFile);
@@ -35,6 +48,8 @@ phi = 0;
 freq = WHITE;
 TS = 1/(2*START_FREQ);
 time = 0;
+
+
 
 for i = 1:size(start_signal,2)
     if (time + WAV_TS <= TS)
@@ -66,7 +81,7 @@ for i=1:7600
     phi = phi + 2*pi*BLACK*WAV_TS;
 end
 for i=7601:8000
-   chirp(i) = 0.99*cos(phi)
+   chirp(i) = 0.99*cos(phi);
    phi = phi +2*pi*WHITE*WAV_TS;
 end
 chirp = repmat(chirp,40,1);
@@ -75,18 +90,23 @@ wavwrite(chirp,WAV_FS,WAV_NBITS,'chirp.wav');
 %% Modulate Image
 phi = 0;
 
-t = 0:WAV_TS:(9*WAV_TS);
+t = 0:WAV_TS:((SAMP_PIX - 1)*WAV_TS);
 
-wav_data = zeros(1, 10*width*len);
+wav_data = zeros(1, SAMP_PIX*width*len);
 for i = 1 : len
     for j = 1 : width
         freq = BLACK + (WHITE-BLACK) * (img(i,j) / 255);
-        for k = 1 : 10
-           wav_data(10*((i-1)*width + (j-1)) + (k)) = 0.99*cos(2*pi*freq*t(k) + phi);
+        for k = 1 : SAMP_PIX
+           wav_data(SAMP_PIX*((i-1)*width + (j-1)) + (k)) = ...
+               0.99*cos(2*pi*freq*t(k) + phi);
         end
-        phi = phi+(2*pi*freq*WAV_TS*10);
+        phi = phi+(2*pi*freq*WAV_TS*SAMP_PIX);
     end
 end
+
+%% Generate Stop Signal
+stop_signal = startStop(STOP_FREQ, STOP_TIME);
+wavwrite(stop_signal, WAV_FS, WAV_NBITS, 'stop.wav');
 
 %% Write to WAVE file
 wavwrite(wav_data, WAV_FS, WAV_NBITS, wavFile);
